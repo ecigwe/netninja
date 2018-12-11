72ac72637b6c394a03ab6fed31c90caaf1bbfc8f
emmanuel <emmanuel.igwe38@gmail.com>
2018-12-11 18:13:28 +0100

    sign up

--- src/components/project/ProjectList.js
+++ src/components/project/ProjectList.js
@@ -1,4 +1,4 @@
-import React, { Component } from 'react';
+import React from 'react';
 import ProjectSummary from './ProjectSummary'
 
 const ProjectList = () => {
--- src/components/project/ProjectSummary.js
+++ src/components/project/ProjectSummary.js
@@ -1,4 +1,4 @@
-import React, { Component } from 'react'
+import React from 'react'
 
 const ProjectSummary = () => {
     return(
