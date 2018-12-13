088f6a59f58884d493944f370691ea88dd00af45
emmanuel <emmanuel.igwe38@gmail.com>
2018-12-13 15:57:20 +0100

    reducer2

--- src/index.js
+++ src/index.js
@@ -3,9 +3,14 @@
  import './index.css';
  import App from './App';
  import registerServiceWorker from './registerServiceWorker';
+ import { createStore } from 'redux'
+ import rootReducer from './store/reducers/rootReducer'
+ import { Provider } from 'react-redux'
+
+
  
 
+const store = createStore(rootReducer);
 
-
- ReactDOM.render(<App />, document.getElementById('root'));
+ ReactDOM.render(<Provider store={store}><App /></Provider>, document.getElementById('root'));
  registerServiceWorker();
---
+++ src/store/reducers/rootReducer.js
@@ -0,0 +1,12 @@
+import authReducer from './authReducer'
+import projectReducer from './projectReducer'
+import { combineReducers } from 'redux'
+
+
+const rootReducer = combineReducers({
+    auth: authReducer,
+    project: projectReducer
+})
+
+
+export default rootReducer
